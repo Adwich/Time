1184af4f3f4ca25386663e8dcf0b23a917b957ef
Adwich <adel.riffi@gmail.com>
2016-11-01 18:04:24 +0100

    Supporting CORS for localhost request

--- time.js
+++ time.js
@@ -32,6 +32,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 
 
 var router = express.Router();
